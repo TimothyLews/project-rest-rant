2ae2b4d4fc954172208fd2ba52c2b9c0ef1d7edd
TimothyLewis <timlew2003@gmail.com>
2024-09-19 00:49:05 -0700

    FOrm data is getting through

--- controllers/places.js
+++ controllers/places.js
@@ -7,14 +7,14 @@ router.get('/', (req, res) => {
             city: 'Seattle',
             state: 'WA',
             cuisines: 'Thai, Pan-Asian',
-            pic: 'http://placekitten.com/250/250'
+            pic: 'https://unsplash.com/photos/brown-tabby-kitten-sitting-on-floor-nKC772R_qog'
         },
         {
             name: 'Coding Cat Cafe',
             city: 'Phoenix',
             state: 'AZ',
             cuisines: 'Coffee, Bakery',
-            pic: 'http://placekitten.com/250/250'
+            pic: 'https://unsplash.com/photos/brown-tabby-kitten-sitting-on-floor-nKC772R_qog'
         }
     ];
     res.render('places/index', { places })
@@ -24,5 +24,9 @@ router.get('/', (req, res) => {
 router.get('/new', (req, res) => {
   res.render('places/new'); 
 });
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send('POST /places')
+})
 
 module.exports = router
