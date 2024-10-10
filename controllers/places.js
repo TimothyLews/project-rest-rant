f37e8cdc29f5aa8457149a7e69286cd0d5bfe06d
TimothyLewis <timlew2003@gmail.com>
2024-10-10 13:54:13 -0700

    code has been reset to what it was before the most recent commits

--- controllers/places.js
+++ controllers/places.js
@@ -1,9 +1,54 @@
 const router = require('express').Router()
+const places = require('../models/places.js')
 
 router.get('/', (req, res) => {
-  res.send('GET /places')
-})
+  let places = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/h-thai-ml-tables.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/coffe-cat.jpg'
+  }]
+  
+    res.render('places/index', { places })
+});
 
+// Route to render the new place form
+router.get('/new', (req, res) => {
+  res.render('places/new'); 
+});
+router.post('/', (req, res) => {
+  console.log(req.body)
+  if (!req.body.pic) {
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
+})
+router.get('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }
+  else {
+    res.render('places/show', {place: places[id]})
+  }
+})
 
 
 module.exports = router
