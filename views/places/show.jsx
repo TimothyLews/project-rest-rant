a7a7d86274bd70b598328c02b063fe079f75082a
TimothyLewis <timlew2003@gmail.com>
2024-10-13 15:47:32 -0700

    changing the way data is displayed

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,24 +1,28 @@
 const React = require("react");
 const Def = require("../default");
 
-function show(data) {
+function show(props) {
   return (
     <Def>
       <main>
         <br/><br/>
         <div className="row">
           <div className="col-sm-6">
-            <img id="show-image" src={data.place.pic} alt={data.place.name} />
+            <img id="show-image" src={props.place.pic} alt={props.place.name} />
           </div>
           <div className="col-sm-6">
-            <h1>{data.place.name}</h1>
+            <h1>{props.place.name}</h1>
             <h2>Rated</h2>
             <p>Not Rated</p>
             <h2>Description</h2>
-            <p>
-              Located {data.place.city}, {data.place.state}, and serving{" "}
-              {data.place.cuisines}
-            </p>
+            <h2>
+              {props.place.showEstablished()}
+            </h2>
+
+            <h3>
+              Serving {props.place.cuisines}
+            </h3>
+
           </div>
         </div>
         <br/><br/><br/>
@@ -29,13 +33,13 @@ function show(data) {
         <br/><br/>
         <div className="row">
           <a className="col-sm-1 edit-button"
-            href={`/places/${data.id}/edit`}
+            href={`/places/${props.place.id}/edit`}
           >
             Edit
           </a>
           <form
             method="POST"
-            action={`/places/${data.id}?_method=DELETE`}
+            action={`/places/${props.place.id}?_method=DELETE`}
             className="col-sm-1 delete-form"
           >
             <button type="submit" className="btn btn-danger delete-button">
