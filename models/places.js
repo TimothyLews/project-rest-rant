9199b6d230aa21cf01629266ee838d5156071e48
TimothyLewis <timlew2003@gmail.com>
2024-10-13 16:11:20 -0700

    comments model and seeder file have been added and updated

--- models/places.js
+++ models/places.js
@@ -10,7 +10,8 @@ const placeSchema = new mongoose.Schema({
   type: Number,
   min: [1673, 'Surely not that old?!'],
   max: [new Date().getFullYear(), 'The future is now!'] 
-  }
+  },
+  comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
 })
 
 placeSchema.methods.showEstablished = function() {
