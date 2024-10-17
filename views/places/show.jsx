e33382e422c2ff27bd669a12ca2c7f3fbad90761
TimothyLewis <timlew2003@gmail.com>
2024-10-16 20:29:14 -0700

    Refactor show function to resolve merge conflicts and clean up code

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,54 +1,6 @@
 const React = require("react");
 const Def = require("../default");
 
-<<<<<<< HEAD
-function show (data) {
-  let comments = (
-    <h3 className="inactive">
-      No comments yet!
-    </h3>
-  )
-  let rating = (
-    <h3 className="inactive">
-      Not yet rated
-    </h3>
-  )
-  if (data.place.comments.length) {
-    let sumRatings = data.place.comments.reduce((tot, c) => {
-      return tot + c.stars
-    }, 0)
-    let averageRating = Math.round(sumRatings / data.place.comments.length)
-    let stars = ''
-    for (let i = 0; i < averageRating; i++) {
-      stars += 'â­ï¸'
-    }
-    rating = (
-      <h3>
-        {stars} stars
-      </h3>
-    )
-  }
-  return (
-      <Def>
-        <main>
-          <div className="row">
-            ...
-            <div className="col-sm-6">
-              <h1>{ data.place.name }</h1>
-              <h2>
-                Rating
-              </h2>
-              {rating}
-              <br />
-              ...
-            </div>
-          </div>
-        </main>
-      </Def>
-    )
-  }
-
-=======
 function show(data) {
   let comments = <h3 className="inactive">No comments yet!</h3>;
   let rating = <h3 className="inactive">Not yet rated</h3>;
@@ -187,6 +139,5 @@ function show(data) {
     </Def>
   );
 }
->>>>>>> 0880c77cc9ca98cb87fb54fb34d2bce78b5ae10a
 
 module.exports = show;
