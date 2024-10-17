866b51fee3d9ed0fbd215a54131271e0b45dbea3
TimothyLewis <timlew2003@gmail.com>
2024-10-16 19:22:44 -0700

    Refactor index component to resolve merge conflicts and enhance code consistency

--- views/places/index.jsx
+++ views/places/index.jsx
@@ -1,53 +1,33 @@
 const React = require("react");
 const Def = require("../default.jsx");
 
-function index (data) {
-<<<<<<< HEAD
-    let placesFormatted = data.places.map((place) => {
-        return (
-            <div classname="col-sm-6">
-                <h2>
-                    <a href={'/places/${place.id}'}>
-                    {place.name}
-                    </a>
-                </h2>
-                <p clasName="text-center">
-                    {place.cuisines}
-                </p>
-                <img src={place.pic} alt={place.name}/>
-                <p className="text-center">
-                    Located in {place.city}, {place.state}
-                </p>
-            </div>
-        )
-    })
+const React = require("react");
+const Def = require("../default.jsx");
 
-=======
+function index(data) {
   let placesFormatted = data.places.map((place) => {
->>>>>>> 0880c77cc9ca98cb87fb54fb34d2bce78b5ae10a
     return (
-      <div className="col-sm-6">
-        <h2>{place.name}</h2>
-        <p className="text-center">
-          {place.cuisines}
-        </p>
-        <img src={place.pic} alt={place.name} />
-        <p className="text-center">
-          Located in {place.city}, {place.state}
-        </p>
-      </div>
-    )
-  })
-    return (
+      
+        <div className="col-sm-6">
+          <h2>
+            <a href={`/places/${place.id}`}>{place.name}</a>
+          </h2>
+          <p className="text-center">{place.cuisines}</p>
+          <img className="places-image" src={place.pic} alt={place.name} />
+          <p className="text-center">
+            Located in {place.city}, {place.state}
+          </p>
+        </div>
+    );
+  });
+  return (
     <Def>
-        <main>
-            <h1>Places to Rant or Rave About</h1>
-            <div className="row">
-              {placesFormatted}
-            </div>
-        </main>
+      <main>
+        <h1>Places to Rant or Rave About</h1>
+        <div className="row">{placesFormatted}</div>
+      </main>
     </Def>
-  )
+  );
 }
 
 module.exports = index;
