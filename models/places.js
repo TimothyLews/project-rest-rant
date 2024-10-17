5ad07d08a30834debdbd6fcea49a25b3071699ea
TimothyLewis <timlew2003@gmail.com>
2024-10-16 19:14:01 -0700

    Refactored place Schema to resolve murge conflicts

--- models/places.js
+++ models/places.js
@@ -1,41 +1,23 @@
 const mongoose = require('mongoose')
 
+
 const placeSchema = new mongoose.Schema({
-<<<<<<< HEAD
-    name: { type: String, required: true },
-    pic: { type: String, default: 'http://placekitten.com/350/350'},
-    cuisines: { type: String, required: true },
-    city: { type: String, default: 'Anytown' },
-    state: { type: String, default: 'USA' },
-    founded: {
-      type: Number,
-      min: [1673, 'Surely not that old?!'],
-      max: [new Date().getFullYear(), 'This is the future!']
-    },
-    comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
-  })
-  
-  
-
-placeSchema.methods.showEstablished = function() {}
-
-module.exports = mongoose.model('Place', placeSchema)
-=======
   name: { type: String, required: true },
-  pic: {type: String, defualt: 'http://placekitten.com/400/400'},
+  pic: { type: String, default: 'http://placekitten.com/350/350'},
   cuisines: { type: String, required: true },
   city: { type: String, default: 'Anytown' },
   state: { type: String, default: 'USA' },
-  founded:{ 
-  type: Number,
-  min: [1673, 'Surely not that old?!'],
-  max: [new Date().getFullYear(), 'The future is now!'] 
+  founded: {
+    type: Number,
+    min: [1673, 'Surely not that old?!'],
+    max: [new Date().getFullYear(), 'This is the future!']
   },
-  comments: [{type: mongoose.Schema.Types.ObjectId, ref: 'Comment'}]
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Comment' }]
 })
 
 placeSchema.methods.showEstablished = function() {
-  return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
+ return `${this.name} has been serving ${this.city}, ${this.state} since ${this.founded}.`
 }
+
+
 module.exports = mongoose.model('Place', placeSchema)
->>>>>>> 0880c77cc9ca98cb87fb54fb34d2bce78b5ae10a
