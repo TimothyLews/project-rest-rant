2c99dc60d536a8e576b32d5a460cb455b430fabc
TimothyLewis <timlew2003@gmail.com>
2024-10-16 19:18:06 -0700

    Refactored Def component to resolve merge conflicts and update structure

--- views/default.jsx
+++ views/default.jsx
@@ -1,41 +1,34 @@
-const React = require('react')
+const React = require("react");
 
-function Def (html) {
-    return (
-        <html>
-            <head>
-                <title>Title</title>
-<<<<<<< HEAD
-                <link rel="stylesheet" href="public/style.css" />
-                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" />
-=======
-                <link 
-                rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" 
-                />
-                <link rel="stylesheet" href="/css/style.css" />
+function Def(html) {
+  return (
+    <html>
+      <head>
+        <title>REST-Rant!</title>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css"
+        />
+        <link rel="stylesheet" href="/css/style.css" />
+      </head>
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="/places">Places</a>
+            </li>
+            <li>
+              <a href="/places/new">Add Place</a>
+            </li>
+          </ul>
+        </nav>
+        {html.children}
+      </body>
+    </html>
+  );
+}
 
-
->>>>>>> 0880c77cc9ca98cb87fb54fb34d2bce78b5ae10a
-            </head>
-            <body>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href="/">Home</a>
-                        </li>
-                        <li>
-                            <a href="/places">Places</a>
-                        </li>
-                        <li>
-                            <a href="/places/new">Add Place</a>
-                        </li>
-                    </ul>
-                </nav>
-                {html.children}
-            </body>
-        </html>
-    )
-  }
-  
-
-module.exports = Def
+module.exports = Def;
